4a92c852c6e80685dd98948294587c646ec27938
test <esfahani.aliyahoo@gmail.com>
2023-11-26 16:01:36 +0330

    configure tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
-
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 export default function Home() {
-  
-
   return (
-    <div className="flex items-center justify-center md:flex-col">
-      <p>alsdjf</p>
-      <span>
-        <p>
-          alskdjf <span>alkjsdf</span>
-        </p>
-      </span>
+    <div className="flex items-center justify-center md:flex-col ">
+      <p>lajdf</p>
     </div>
   );
 }
