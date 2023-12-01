ae9e2611c73d5b39c0380a92b780d7311ad7c7c0
test <esfahani.aliyahoo@gmail.com>
2023-12-01 15:30:49 +0330

    fix dark/light mode localsearch component

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,21 +1,22 @@
 "use client";
 import { HomePageFilters } from "@/constants/filter";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 
 const HomeFilters = () => {
-  const isActive = "";
+  const [active, setActive] = useState("");
+
 
   return (
     <div className="mt-10  flex-wrap gap-3 font-inter md:flex">
       {HomePageFilters.map((filter, index) => (
         <Button
           key={index}
-          onClick={() => {}}
-          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
-            isActive === filter.value
-              ? "bg-primary-100 text-primary-500"
-              : "bg-light-800 text-light-500"
+          onClick={() => setActive(filter.value)}
+          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none  ${
+            active === filter.value
+              ? "bg-primary-100 text-primary-500 dark:bg-dark-500"
+              : " bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300 "
           }`}
         >
           {filter.name}
